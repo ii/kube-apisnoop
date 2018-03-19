71ba90af825c8a0949ead657c22c0179164d51b6
Chris McClimans <chris@hippiehacker.org>
2018-03-20 04:59:35 +1300

    updates

--- cmd/tproxy-initializer/main.go
+++ cmd/tproxy-initializer/main.go
@@ -1,5 +1,3 @@
-// +k8s:deepcopy-gen=package,register
-// +k8s:deepcopy-gen=true
 // Copyright 2017 Google Inc. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
