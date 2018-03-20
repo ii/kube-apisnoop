5a8f179cc3b04849783c2ac673dfcef78de90f49
Chris McClimans <chris@hippiehacker.org>
2018-03-21 07:26:10 +1300

    generate k8s csr/cert for mitm.ca

--- create_kubeapi_crt.sh
+++ create_kubeapi_crt.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-CSR_NAME=tproxy-kubernetes3.ii
+CSR_NAME=k8s-mitm.ii
 
 mkdir -p fakecerts
 cd fakecerts
 echo Generating key
-openssl genrsa -out server.key 2048
+openssl genrsa -out mitm.key 2048
+echo Generating k8s-ca.pem
+kubectl config view --minify --flatten=true \
+    | grep certificate-authority-data | awk -F\  '{print $2}' \
+    | base64 -d > k8s-ca.pem
+#openssl x509 -text -noout -in ./k8s-ca.pem
 echo Generating csr request
-openssl req -new -key server.key -out server.csr -config ../csr.settings
-
+#cat k8s-ca.pem | openssl x509 -x509toreq -signkey mitm.key -out k8s.csr
+openssl req -new -key mitm.key -out mitm.csr -config ../csr.settings
 
 echo Adding CSR to Kubernetes
 cat <<EOF | kubectl create -f -
@@ -19,7 +24,7 @@ metadata:
 spec:
   groups:
   - system:authenticated
-  request: $(cat server.csr | base64 | tr -d '\n')
+  request: $(cat mitm.csr | base64 | tr -d '\n')
   usages:
   - digital signature
   - key encipherment
@@ -30,6 +35,6 @@ echo Approving CSR
 kubectl certificate approve $CSR_NAME
 echo Getting result cert
 sleep 3
-kubectl get csr $CSR_NAME -o jsonpath='{.status.certificate}' | base64 -d > server.crt
+kubectl get csr $CSR_NAME -o jsonpath='{.status.certificate}' | base64 -d > mitm.crt
 echo Done. Heres the result
-openssl x509  -noout -text -in ./server.crt
+openssl x509  -noout -text -in ./mitm.crt
