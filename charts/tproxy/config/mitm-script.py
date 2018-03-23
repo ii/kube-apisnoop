8d51a0cc634415330e665afaaffd0d83b72b722e
Rohan Fletcher <rohan@ii.coop>
2018-03-23 13:59:05 -0700

    Return immediately in mitmproxy script temporarily

--- charts/tproxy/config/mitm-script.py
+++ charts/tproxy/config/mitm-script.py
@@ -7,6 +7,7 @@ ALLOW_BUCKET_NAME = "solutions-public-assets"
 RE_BUCKET = re.compile(r'https://storage.googleapis.com/%s/.*' % ALLOW_BUCKET_NAME)
 
 def request(flow: http.HTTPFlow) -> None:
+    return
     if not RE_BUCKET.match(flow.request.pretty_url):
         flow.response = http.HTTPResponse.make(
             418,
