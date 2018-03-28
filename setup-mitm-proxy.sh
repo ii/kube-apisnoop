76da9721355f990780230b5b26a9a5f9f45fb19e
Rohan Fletcher <rohan@ii.coop>
2018-03-27 17:17:21 -0700

    Tidy up old code a bit

--- setup-mitm-proxy.sh
+++ setup-mitm-proxy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 INSTALL_NAME=tproxy
-ACCOUNT_NAME="${INSTALL_NAME}-mitm"
-ACCOUNT_SECRET_NAME="${ACCOUNT_NAME}-secret"
 
 cd charts/tproxy
+
 # if certificates havent been created yet, create them
 if [ ! -d "$(pwd)/certs" ]; then
     echo "Certs not found, creating certs"
@@ -12,15 +11,12 @@ if [ ! -d "$(pwd)/certs" ]; then
 fi
 # load the fake CA cert for injection into the mitm service account
 MITMPROXY_CERT=$(cat ./certs/mitmproxy-ca-cert.pem | base64 -w 0)
+
+
 # Run the install with useInitializer=true
 echo "Installing tproxy using helm...."
-helm install -n $INSTALL_NAME --set tproxy.useInitializer=true --set tproxy.accountName="$ACCOUNT_NAME" --set tproxy.accountSecretName="$ACCOUNT_SECRET_NAME" .
+helm install -n $INSTALL_NAME --set tproxy.useInitializer=true .
 cd -
 
 # Wait a second
 sleep 1
-
-# One liner to get the secrets, patch the CA, then apply changes
-# kubectl get secrets/$ACCOUNT_SECRET_NAME -o yaml | sed "s/ca.crt:.\+/ca.crt: $MITMPROXY_CERT/" | kubectl apply -f -
-
-# kubectl logs -l app=tproxy -c tproxy-podwatch &
